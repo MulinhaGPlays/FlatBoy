19dc30dde9854eba0ca9b1348358ef60c3dfbfab
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2023-06-01 06:52:18 -0300

    correr

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ var flatBoy = document.getElementById("flatboy");
 var isKeyPressed = false;
 
 document.addEventListener("keydown", function(event) {
-  if (!isKeyPressed && event.key == "d" || event.key == "ArrowLeft" || event.key == "D") {
+  if (!isKeyPressed && event.key == "d" || event.key == "ArrowRight" || event.key == "D") {
     isKeyPressed = true;
     flatBoy.classList.remove("stopped");
     flatBoy.classList.add("walking");
+  }
+  if (!isKeyPressed && event.key == "w" || event.key == "ArrowUp" || event.key == "W") {
+    isKeyPressed = true;
+    flatBoy.classList.remove("stopped");
+    flatBoy.classList.add("running");
   }
 });
 
 document.addEventListener("keyup", function(event) {
   isKeyPressed = false;
+    flatBoy.classList.remove("running");
     flatBoy.classList.remove("walking");
     flatBoy.classList.add("stopped");
 });
